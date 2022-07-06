81c88dc4123ec7e86f87ecdaaea042a679ced54d
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-06 16:51:00 -0300

    Finalizado

--- Formacao_C_Sharp/Entendo_Finaly/Program.cs
+++ Formacao_C_Sharp/Entendo_Finaly/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Entendo_Finaly
 {
@@ -6,13 +7,41 @@ namespace Entendo_Finaly
     {
         static void Main(string[] args)
         {
+            LeitorDeArquivos l = null;
 
+            try
+            {
+                l.LerProximaLinha();
+                l.LerProximaLinha();
+                l.LerProximaLinha();
+            }
+            catch (NullReferenceException ex)
+            {
+                 Console.WriteLine("Erro");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Exceção do tipo IOException capturada e tratada.");
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Erro");
+            }
+            finally
+            {
+                if (l != null)
+                {
+                    l.Fechar();
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 
     public class LeitorDeArquivos
     {
-        public string Arquivo { get;  }
+        public string Arquivo { get; }
 
         public LeitorDeArquivos(string arquivo)
         {
@@ -23,7 +52,8 @@ namespace Entendo_Finaly
         public string LerProximaLinha()
         {
             Console.WriteLine("Lendo linha . . .");
-            return "Linho do arquivo";
+            throw new IOException();
+            return "Linha do arquivo";
         }
 
         public void Fechar()
