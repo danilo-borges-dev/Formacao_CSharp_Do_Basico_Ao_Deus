c5f7cc3b58d2970e78938467701f4c7e9ecc8f26
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-07 14:44:38 -0300

    Trabalhando com substring

---
+++ Formacao_C_Sharp/TrabalhandoComStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+
+            Console.WriteLine(url.Substring(7));
+
+
+            for (int i = 0; i < url.Length; i++)
+            {
+                string s = url[i].ToString();
+                if (s != "?")
+                {
+                    Console.Write(s);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
