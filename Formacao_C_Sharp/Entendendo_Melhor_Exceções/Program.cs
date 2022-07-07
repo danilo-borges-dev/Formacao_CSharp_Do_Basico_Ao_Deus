4dc3ef572bb38c8b0e353739fda1e28fc32a018e
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-07 07:18:34 -0300

    Entendo melhor exceções

---
+++ Formacao_C_Sharp/Entendendo_Melhor_Exceções/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entendendo_Melhor_Exceções
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int resultado = Matematica(10, 0);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            Console.ReadLine();
+        }
+
+        static int Matematica(int numerador, int denominador)
+        {
+            if (denominador == 0)
+            {
+                throw new ArgumentException("Você não pode definir o denominador igual a 0.", nameof(denominador));
+            }
+            return numerador / denominador;
+        }
+    }
+}
