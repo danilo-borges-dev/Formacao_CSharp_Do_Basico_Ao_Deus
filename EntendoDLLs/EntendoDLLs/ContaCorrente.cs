732369ca73380a625ac25b1b3c22bb8a58384f86
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-07 11:57:29 -0300

    Entendendo como documentar um sistema

---
+++ EntendoDLLs/EntendoDLLs/ContaCorrente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntendoDLLs
+{
+    internal class ContaCorrente
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Construtor da classe ContaCorrente, você precisa popular os parêmtros de entrada <see cref="Id"/> e <see cref="Number"/>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="number"></param>
+        public ContaCorrente(int id, int number)
+        {
+            Id = id;
+            Number = number;
+        }
+    }
+}
---
+++ EntendoDLLs/EntendoDLLs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace EntendoDLLs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste");
+            Console.ReadLine();
+
+        }
+    }
+}
