8bdda9aa2ba772933e46db1366bf6066d1833a1d
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-08 17:55:23 -0300

    Melhorando o padrão buscado pela expressão regular

--- Formacao_C_Sharp/TrabalhandoComStrings/Program.cs
+++ Formacao_C_Sharp/TrabalhandoComStrings/Program.cs
@@ -14,11 +14,12 @@ namespace TrabalhandoComStrings
 
             //Console.WriteLine(valida.GetValor("moedaOrigem").ToUpper());
 
-            string mensagem_01 = "Me ligue em 7894-2095";
+            string mensagem_01 = "Me ligue em 97894-2095";
             string mensagem_02 = "Meu número de telefone é 7895-5263";
             string mensagem_03 = "O número de telefone 9915-5236 é contato profissional";
 
             string padraoTelefone = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            padraoTelefone = "[0-9]{4,5}-?[0-9]{4}";
 
             Console.WriteLine(Regex.IsMatch(mensagem_01, padraoTelefone));  // Temos como retorno um true
             Console.WriteLine(Regex.Match(mensagem_01, padraoTelefone)) ;// Temos como retorno o número que está dentro do padrão
