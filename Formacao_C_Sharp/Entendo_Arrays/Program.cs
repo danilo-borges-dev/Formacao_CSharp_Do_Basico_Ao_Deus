5ca4adbc85918f3c15891abf96575a8f017220ec
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-09 07:09:43 -0300

    Entendo o funcionamento de arrays

---
+++ Formacao_C_Sharp/Entendo_Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entendo_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] idades = new int[5];
+
+            idades[0] = 1;
+            idades[1] = 2;
+            idades[2] = 3;
+            idades[3] = 4;
+            idades[4] = 5;
+
+            Console.WriteLine(idades[3]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
