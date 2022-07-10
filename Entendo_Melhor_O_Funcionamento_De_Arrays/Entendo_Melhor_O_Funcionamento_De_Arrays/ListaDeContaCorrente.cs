f3bbaf5a1ac24d05a6d57fbbdc9d0bb6367b0fec
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-10 11:10:19 -0300

    Entendedo a diretiva params nos metodos

---
+++ Entendo_Melhor_O_Funcionamento_De_Arrays/Entendo_Melhor_O_Funcionamento_De_Arrays/ListaDeContaCorrente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entendo_Melhor_O_Funcionamento_De_Arrays
+{
+    internal class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+
+        public ListaDeContaCorrente()
+        {
+            _itens = new ContaCorrente[5];  // temos um construtor que não possui parâmetro inicial de entrada, porém observe que no corpo do construtor definimos o tamanho do array e definimos o valor da variável _próximaPosição
+            _proximaPosicao = 0;
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+
+            Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+        public void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+            Console.WriteLine("Aumentando capacidade da lista.");
+
+            ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+
+            for (int i = 0; i < _itens.Length; i++)
+            {
+                novoArray[i] = _itens[i];
+                Console.WriteLine(".");
+            }
+            _itens = novoArray;
+        }
+    }
+}
--- Entendo_Melhor_O_Funcionamento_De_Arrays/Entendo_Melhor_O_Funcionamento_De_Arrays/Program.cs
+++ Entendo_Melhor_O_Funcionamento_De_Arrays/Entendo_Melhor_O_Funcionamento_De_Arrays/Program.cs
@@ -1,14 +1,15 @@
 ﻿using Entendo_Melhor_O_Funcionamento_De_Arrays;
 
-ContaCorrente[] conta = new ContaCorrente[]
-{
-    new ContaCorrente("Danilo", 29),
-    new ContaCorrente("João", 67),
-    new ContaCorrente("Sueli", 60)
-};
+ListaDeContaCorrente lista = new ListaDeContaCorrente();
+lista.Adicionar(new ContaCorrente("Danilo", 123));
+lista.Adicionar(new ContaCorrente("Danilo", 123));
+lista.Adicionar(new ContaCorrente("Danilo", 123));
+lista.Adicionar(new ContaCorrente("Danilo", 123));
+lista.Adicionar(new ContaCorrente("Danilo", 123));
+lista.Adicionar(new ContaCorrente("Danilo", 123));
 
-for (int i = 0; i < conta.Length; i++)
-{
-    ContaCorrente contaAuxiliar = conta[i];
-    Console.WriteLine($"Conta de {contaAuxiliar.Name}. Idade do Cliente é {contaAuxiliar.Age}");
-}
+//for (int i = 0; i < conta.Length; i++)
+//{
+//    ContaCorrente contaAuxiliar = conta[i];
+//    Console.WriteLine($"Conta de {contaAuxiliar.Name}. Idade do Cliente é {contaAuxiliar.Age}");
+//}
