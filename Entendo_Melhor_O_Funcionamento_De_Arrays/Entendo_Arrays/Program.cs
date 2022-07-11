2626e1c31cd3c3885e083d52fe4185f37e139890
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-11 07:29:33 -0300

    Entendo melhor os metodos e retornos do array

--- Entendo_Melhor_O_Funcionamento_De_Arrays/Entendo_Arrays/Program.cs
+++ Entendo_Melhor_O_Funcionamento_De_Arrays/Entendo_Arrays/Program.cs
@@ -2,3 +2,31 @@
 string[] firtArray = new string[1];
 int[] secondArray = {1, 2, 3};
 bool[] thirdArray = new bool[] {true, false, true};
+short[] fourthArray = null;
+
+string[,] newArray = { {"Danilo", "Sueli", "Marcos" },{"Rosana", "Nayla", "Ysabela" } }; // 
+
+Console.WriteLine($"Acessando o primeiro elemento do primeiro array: {newArray[0,0]}");
+Console.WriteLine($"Acessando o segundo elemento do segundo array: {newArray[1,2]}");
+
+Console.WriteLine(secondArray.IsFixedSize);
+Console.WriteLine(newArray.Rank);
+
+var source = new[] { "Ally", "Bishop", "Billy" };
+var target = new string[5];
+
+source.CopyTo(target, 2);
+
+    int i = 0;
+foreach (var item in target)
+{
+    Console.WriteLine(i);
+    Console.WriteLine(item);
+    i++;
+}
+
+// output:
+
+// Ally
+// Bishop
+// Billy
