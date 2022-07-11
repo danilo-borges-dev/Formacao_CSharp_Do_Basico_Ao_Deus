6863349ffdaa5484a97c56c4af816ac82e04c45f
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-11 16:45:03 -0300

    Entendo ICompare

---
+++ Entendo_Melhor_O_Funcionamento_De_Arrays/ICompare_teste/ComparadorDescrescente.cs
@@ -0,0 +1,19 @@
+﻿namespace ICompare_teste
+{
+    internal class ComparadorDescrescente : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x == y)
+            {
+                return 0;
+            }
+
+            if (x > y)
+            {
+                return 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Entendo_Melhor_O_Funcionamento_De_Arrays/ICompare_teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using ICompare_teste;
+
+List<int> numbers = new List<int>() { 11, 5, 6, 81, 45, 32, 85, 58, 145, 2, 1 };
+ComparadorDescrescente cd = new();
+
+numbers.Sort(cd);
+
+foreach (var item in numbers)
+{
+    Console.WriteLine(item);
+}
+
