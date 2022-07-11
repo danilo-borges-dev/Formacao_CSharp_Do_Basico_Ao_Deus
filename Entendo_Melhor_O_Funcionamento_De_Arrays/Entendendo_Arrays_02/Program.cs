e987942d9f0728f5d469331dbb9745655b8ca248
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-11 10:57:33 -0300

    Entendo a classe Array

--- Entendo_Melhor_O_Funcionamento_De_Arrays/Entendendo_Arrays_02/Program.cs
+++ Entendo_Melhor_O_Funcionamento_De_Arrays/Entendendo_Arrays_02/Program.cs
@@ -15,3 +15,11 @@ contatos[1] = c2;
 Console.WriteLine(contatos[0].Name);
 Console.WriteLine(contatos[1].Age);
 Console.WriteLine(contatos[1].Name);
+
+Console.WriteLine();
+
+int[] numeros = { 1, 2, 3, 4, 5, 6 };
+
+Array a = (Array)numeros;   // Aqui armazenei a referencia do array numeros na variavel a.
+
+Console.WriteLine(a.GetValue(4)); // Com GetValue é possível acessar o valor 
