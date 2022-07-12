a9b2f5c48d3bb65a023345ee07918cfeb13043d6
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-12 07:49:02 -0300

    IEquatable com Struct

---
+++ Entendo_Melhor_O_Funcionamento_De_Arrays/IEquatable_Com_Structs/Cor.cs
@@ -0,0 +1,23 @@
+﻿namespace IEquatable_Com_Structs
+{
+    internal struct Cor : IEquatable<Cor>
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public Cor(int codigo, string nome)
+        {
+            Codigo = codigo;
+            Nome = nome;
+        }
+
+        public bool Equals(Cor other)
+        {
+            if (this.Codigo == other.Codigo)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Entendo_Melhor_O_Funcionamento_De_Arrays/IEquatable_Com_Structs/Program.cs
@@ -0,0 +1,15 @@
+﻿using IEquatable_Com_Structs;
+
+List<Cor> cores = new()
+{
+    new Cor (10, "Amarelo"),
+    new Cor (11, "Azul"),
+    new Cor (78, "Roxo"),
+    new Cor (89, "Vermelho"),
+};
+
+Cor novaCor = new(11, "Azul");
+
+bool result = cores.Contains(novaCor);
+
+Console.WriteLine("Resultado " + result);
