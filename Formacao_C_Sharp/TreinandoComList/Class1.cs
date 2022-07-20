206e838ea269ee159e6d7628460d8bf617e792ee
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-20 07:30:09 -0300

    Alterado o tipo objetc da classe Lista e metodos para generecs T

---
+++ Formacao_C_Sharp/TreinandoComList/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace TreinandoComList
+{
+    internal class Class1
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Class1(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Formacao_C_Sharp/TreinandoComList/Program.cs
@@ -0,0 +1,25 @@
+﻿using TreinandoComList;
+
+List<Class1> list = new()
+{
+   new Class1("A", 10),
+   new Class1("B", 50),
+   new Class1("C", 70),
+   new Class1("A", 70),
+};
+
+int n = list.Count;
+
+Console.WriteLine("Total " + n);
+
+List<Class1> list2 = list.OrderBy(x => x.Name).ToList();
+
+list2.ForEach(x => Console.WriteLine(x.Name));
+
+list2 = list.OrderByDescending(x => x.Name).ToList();
+
+list2.ForEach(x => Console.WriteLine(x.Name));
+
+List<Class1> novaLista = (from Class1 c in list where c.Age == 70 select c).ToList();
+
+novaLista.ForEach(x => Console.WriteLine(x.Age));
