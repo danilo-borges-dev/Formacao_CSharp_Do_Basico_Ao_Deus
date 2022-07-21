219a1b41a017fe610c8fd4f3be7e984a950824d4
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-21 12:46:09 -0300

    Praticando e revisando IEquatable

---
+++ Formacao_C_Sharp/RevisaoIEquatable/Carro.cs
@@ -0,0 +1,17 @@
+﻿namespace RevisaoIEquatable
+{
+    internal class Carro : IEquatable<Carro>
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public bool Equals(Carro? other)
+        {
+            if (this.Modelo == other.Modelo)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Formacao_C_Sharp/RevisaoIEquatable/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using RevisaoIEquatable;
+
+List<Carro> listaDeCarros = new();
+
+Carro c1 = new();
+c1.Marca = "Chevrolet";
+c1.Modelo = "Cruze 2012";
+
+Carro c2 = new();
+c2.Marca = "BMW";
+c2.Modelo = "Radiom";
+
+listaDeCarros.Add(c1);
+listaDeCarros.Add(c2);
+
+Carro c3 = new();
+c3.Marca = "Chevrolet";
+c3.Modelo = "Cruze 2012";
+
+bool retorno = listaDeCarros.Contains(c3);
+
+Console.WriteLine("Sim contem " + retorno);
+
+Console.WriteLine();
+
+Carro c4 = new();
+c4.Marca = "Fiat";
+c4.Modelo = "Hardor";
+
+retorno = listaDeCarros.Contains(c4);
+
+Console.WriteLine("Contem? " + retorno);
